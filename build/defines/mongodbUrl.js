c5a0e5fc0a5c9169ff5bf5d2e45c857f7779ea3a
Kenny D <kennyd@btcex.ch>
2018-08-24 23:25:02 +0800

    remove build folder

--- build/defines/mongodbUrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongodbUrl;
-(function (mongodbUrl) {
-    mongodbUrl["local"] = "localhost";
-    mongodbUrl["cloud"] = "mongodb+srv://dbmanager:eLAvxCeoe5BFSBFg@atms-sznh7.mongodb.net/admin";
-})(mongodbUrl = exports.mongodbUrl || (exports.mongodbUrl = {}));
-//# sourceMappingURL=mongodbUrl.js.map
