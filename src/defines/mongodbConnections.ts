3b064a96cfed90a902a37dcf5fbf55182d57524b
Kenny D <kennyd@btcex.ch>
2018-08-28 14:53:24 +0700

    setup environment when the app start

--- src/defines/mongodbConnections.ts
+++ src/defines/mongodbConnections.ts
@@ -1,4 +1,5 @@
 import { networkTypes } from "./networkTypes";
+import { manualEnvironment } from "./manualEnvironment";
 
 //https://ramlez.com/blog/node-js-typescript-2-x-mongodb-quick-start-2/
 //https://www.npmjs.com/package/@types/mongodb
@@ -12,6 +13,34 @@ enum mongodbConnections
 export class mongodbConnectionExs {
     public static getConnection(network: number) : string {
         var connection = mongodbConnections.local;
+        switch(network)
+        {
+            case networkTypes.localhost:
+            {
+                connection = mongodbConnections.local;
+            }
+            break;
+
+            case networkTypes.testnet:
+            {
+                connection = mongodbConnections.cloudTestnet;
+            }
+            break;
+
+            case networkTypes.mainnet:
+            {
+                connection = mongodbConnections.cloudMainnet;
+            }
+            break;
+        }
+
+        return connection;
+    }
+
+    public static getDefaultConnection() : string {
+        var connection = mongodbConnections.local;
+        var network = manualEnvironment.getNetwork();
+        console.log(`==============> get default connection with network: ${network}`);
         switch(network)
         {
             case networkTypes.localhost:
